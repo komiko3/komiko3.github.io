8ab46c079e6261b824f2e96055de4f37bc494af5
komiko3 <komi13104@gmail.com>
2018-09-20 15:34:27 +0900

    add

--- cgi-bin/CommentAnalyzer.py
+++ cgi-bin/CommentAnalyzer.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+
 import cgi
 
 def main():
     form=cgi.FiledStorage()
     pos=form.getValue("pos","")
+    print("Content-type:text/html\n")
     print("<html><body></body></html>")
 
         
