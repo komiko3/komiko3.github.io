b45d93ac42b4d1ad6dd3f9c128cb4ff25d65a557
komiko3 <komi13104@gmail.com>
2018-09-20 17:01:03 +0900

    add

--- js/viewer.js
+++ js/viewer.js
@@ -1,11 +1,9 @@
-var p=document.createElement('p')
-p.innerHTML="hello";
-document.body.appendChild(p);
-/*
 var filename="/Resources/2018-07-21 13-30-56.txt";
 var reader=new FileReader();
 reader.readAsText(filename);
 reader.onload=function(ev){
-    alert(reader.result)
+    alert("hello");
+    var p=document.createElement('p')
+    p.innerHTML=reader.result;
+    document.body.appendChild(p);
 }
-*/
