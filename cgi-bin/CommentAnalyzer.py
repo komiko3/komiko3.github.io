57da5fb0c3b3d79b2b57902fddb0380ca6669d10
komiko3 <komi13104@gmail.com>
2018-09-20 14:49:41 +0900

    add

---
+++ cgi-bin/CommentAnalyzer.py
@@ -0,0 +1,16 @@
+import cgi
+
+class CommentAnalyzer():
+    def init(self):
+        self.text="a"
+
+    def exec(self):
+        a=1
+
+def main():
+    form=cgi.FiledStorage()
+    
+
+        
+if __name__=="__main__":
+    main()
